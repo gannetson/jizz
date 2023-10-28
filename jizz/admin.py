1149ae03eb6518532f3fac2b93d3530858464af2
Loek van Gent <loek@goodup.com>
2023-10-28 14:46:02 +0200

    fix

--- jizz/admin.py
+++ jizz/admin.py
@@ -32,6 +32,8 @@ class CountryAdmin(admin.ModelAdmin):
         return f'{obj.species.count()} species'
 
     def sync_link(self, obj):
+        if not obj:
+            return '-'
         sync_url = reverse('admin:sync-species', args=(obj.pk,))
         return format_html('<a href="{}">Synchronise species</a>', sync_url)
 

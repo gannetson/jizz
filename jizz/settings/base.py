2b762295617010c4adc141490d390ebfb96d0988
Loek van Gent <loek@goodup.com>
2024-09-28 16:53:05 +0200

    stick with sqlite

--- jizz/settings/base.py
+++ jizz/settings/base.py
@@ -86,9 +86,8 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jizz',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

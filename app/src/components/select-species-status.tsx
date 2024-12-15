93fbc770a460d1e0bcd1242a768e81861fa12347
Loek van Gent <loek@goodup.com>
2024-12-15 14:18:08 +0100

    Translations

--- app/src/components/select-species-status.tsx
+++ app/src/components/select-species-status.tsx
@@ -10,22 +10,24 @@ export const SelectSpeciesStatus = () => {
 
   return (
     <Box>
-      <Heading size={'md'} mb={4}>Species status</Heading>
+      <Heading size={'md'} mb={4}>
+        <FormattedMessage defaultMessage={'Species status'} id={'Species status'}/>
+      </Heading>
       <Flex direction={'column'} gap={4}>
-      <Checkbox
-        colorScheme={'orange'}
-        isChecked={includeRare}
-        onChange={(val) => setIncludeRare(Boolean(val.target.checked))}
-      >
-        <FormattedMessage defaultMessage={'Include rare species'} id={'include rare species'}/>
-      </Checkbox>
-      <Checkbox
-        colorScheme={'orange'}
-        isChecked={includeEscapes}
-        onChange={(val) => setIncludeEscapes(Boolean(val.target.checked))}
-      >
-        <FormattedMessage defaultMessage={'Include introduced & escaped species '} id={'include escaped species'}/>
-      </Checkbox>
+        <Checkbox
+          colorScheme={'orange'}
+          isChecked={includeRare}
+          onChange={(val) => setIncludeRare(Boolean(val.target.checked))}
+        >
+          <FormattedMessage defaultMessage={'Include rare species'} id={'include rare species'}/>
+        </Checkbox>
+        <Checkbox
+          colorScheme={'orange'}
+          isChecked={includeEscapes}
+          onChange={(val) => setIncludeEscapes(Boolean(val.target.checked))}
+        >
+          <FormattedMessage defaultMessage={'Include introduced & escaped species '} id={'include escaped species'}/>
+        </Checkbox>
       </Flex>
     </Box>
   )

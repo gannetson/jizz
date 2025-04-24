d212c632ce0fb86080fc157fe40485ccc19e8031
Loek van Gent <loek@goodup.com>
2025-04-24 09:16:13 +0200

    Add app stuff

---
+++ app/android/app/src/main/java/pro/birdr/MainActivity.java
@@ -0,0 +1,5 @@
+package pro.birdr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pro.birdr',
+  appName: 'Birdr - Bird ID Quiz',
+  webDir: 'build'
+};
+
+export default config;

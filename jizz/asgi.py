2f5131139ea0a3d605dd17d05a042aa1de08125e
Loek van Gent <loek@goodup.com>
2026-06-13 14:21:09 +0200

    fix

--- jizz/asgi.py
+++ jizz/asgi.py
@@ -1,14 +1,19 @@
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from jizz.consumers import QuizConsumer  # Adjust the import as necessary
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jizz.settings.production")
 
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+
+from jizz.consumers import QuizConsumer
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter([
             path("mpg/<str:game_token>", QuizConsumer.as_asgi()),

9c99d6f57b47a2f977d742fb81c79d9c79ea907b
Loek van Gent <hallo@loekvan.gent>
2026-09-09 12:58:46 +0200

    Orinoco Goose

--- mobile/android/app/proguard-rules.pro
+++ mobile/android/app/proguard-rules.pro
@@ -12,16 +12,39 @@
 -keep class com.swmansion.worklets.** { *; }
 -keep class com.facebook.react.turbomodule.** { *; }
 
-# expo-image / Glide (release minify). Missing keeps can swallow load callbacks
-# so the quiz feather never ends.
+# Expo media. Debug works; release R8 strips VideoPlayer/AudioPlayer/Glide
+# callbacks (images hang, video/sound crash). expo-video/audio ship no consumer rules.
 -keep class expo.modules.image.** { *; }
+-keep class expo.modules.video.** { *; }
+-keep class expo.modules.audio.** { *; }
+-keep class expo.modules.kotlin.** { *; }
+-keep class * extends expo.modules.kotlin.sharedobjects.SharedObject { *; }
+-keepclassmembers enum expo.modules.** { *; }
+
 -keep class com.bumptech.glide.** { *; }
 -keep public class * implements com.bumptech.glide.module.GlideModule
--keep class * extends com.bumptech.glide.module.AppGlideModule
+-keep class * extends com.bumptech.glide.module.AppGlideModule {
+ <init>(...);
+}
 -keep public enum com.bumptech.glide.load.ImageHeaderParser$** {
   **[] $VALUES;
   public *;
 }
+-keep class com.bumptech.glide.load.data.ParcelFileDescriptorRewinder$InternalRewinder {
+  *** rewind();
+}
+-keep public class com.bumptech.glide.request.ThumbnailRequestCoordinator { *; }
+-keep class com.bumptech.glide.GeneratedAppGlideModuleImpl
+-dontwarn com.bumptech.glide.load.resource.bitmap.VideoDecoder
+
+-keep class androidx.media3.** { *; }
+-dontwarn androidx.media3.**
+-dontwarn android.media.metrics.**
+
+-keep class okhttp3.** { *; }
+-keep class okio.** { *; }
+-dontwarn okhttp3.**
+-dontwarn okio.**
 
 # react-native-svg (QR codes)
 -keep public class com.horcrux.svg.** { *; }
--- mobile/plugins/withAndroidR8.js
+++ mobile/plugins/withAndroidR8.js
@@ -1,6 +1,9 @@
 /**
  * Enable R8 minify/obfuscate/optimize and resource shrinking in Android
  * release builds, so Play Console DEX obfuscation stays above 25%.
+ *
+ * Do not enable R8 fullMode: it strips Expo VideoPlayer/AudioPlayer and
+ * hangs Glide, so production/beta cannot load quiz media.
  */
 const { withAppBuildGradle, withGradleProperties, withDangerousMod } = require('@expo/config-plugins');
 const fs = require('fs');
@@ -9,12 +12,19 @@ const path = require('path');
 const GRADLE_PROPS = {
   'android.enableMinifyInReleaseBuilds': 'true',
   'android.enableShrinkResourcesInReleaseBuilds': 'true',
-  'android.enableR8.fullMode': 'true',
+  'android.enableR8.fullMode': 'false',
 };
 
 const SVG_KEEP = '-keep public class com.horcrux.svg.** { *; }';
 const WORKLETS_KEEP = '-keep class com.swmansion.worklets.** { *; }';
 const HERMES_KEEP = '-keep class com.facebook.hermes.** { *; }\n-keep class com.facebook.jni.** { *; }';
+const EXPO_MEDIA_KEEP = [
+  '-keep class expo.modules.image.** { *; }',
+  '-keep class expo.modules.video.** { *; }',
+  '-keep class expo.modules.audio.** { *; }',
+  '-keep class expo.modules.kotlin.** { *; }',
+  '-keep class * extends expo.modules.kotlin.sharedobjects.SharedObject { *; }',
+].join('\n');
 
 function setGradleProperty(properties, key, value) {
   const existing = properties.find((item) => item.type === 'property' && item.key === key);
@@ -47,7 +57,7 @@ function withAndroidR8(config) {
       const rulesPath = path.join(c.modRequest.platformProjectRoot, 'app', 'proguard-rules.pro');
       if (!fs.existsSync(rulesPath)) return c;
       let rules = fs.readFileSync(rulesPath, 'utf8');
-      const extras = [SVG_KEEP, WORKLETS_KEEP, HERMES_KEEP];
+      const extras = [SVG_KEEP, WORKLETS_KEEP, HERMES_KEEP, EXPO_MEDIA_KEEP];
       for (const line of extras) {
         const first = line.split('\n')[0];
         if (!rules.includes(first)) {
